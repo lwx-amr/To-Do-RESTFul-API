e1fe8675176eea8d7ace04746b504a79fed89ea3
amrister <amr.ister20@gmail.com>
2019-10-18 04:05:35 +0200

    Database connection established (using mongocloud.com)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,32 @@
+// Dummy Data
 var data = [{item: 'get milk'},{item: 'Walk dog'},{item: 'Kick some coding ass'}];
 
+// Body Parser to handle POST requests
 const bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+//Database part
+const mongoose = require('mongoose');
+
+// Connect to database
+mongoose.connect('mongodb+srv://amrister:mngbfhty64@todo-xp4ta.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Create a Schema - like a blue print for our data
+var todoSchema = new mongoose.Schema({
+  item: String,
+});
+
+//Create Model
+var Todo = mongoose.model('Todo',todoSchema);
+
+// Add First Item
+var itemOne = Todo({item: 'get milk'}).save(function(err){
+  if(err) throw err;
+  console.log('Item Saved!!');
+});
 
 module.exports = function (app) {
 
